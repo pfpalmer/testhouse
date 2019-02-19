08a18da0bf42c6d9395a1bff2225d98f7d43d2f1
pfpalmer <pfpalmer@gmail.com>
2019-02-19 11:44:22 -0600

    test class info

--- rgclass.py
+++ rgclass.py
@@ -1,8 +1,22 @@
+import pexpect
+import sys
+from time import sleep
+
 class  gateway:
     serialNumer=0
 
 class nvg599(gateway):
-    pass
+    def __init__(self):
+        rg599 = pexpect.spawn("telnet 192.168.1.254")
+        sleep(1)
+        self.name="abc"
 
-class nvg5268:
-    pass
+    def login(self):
+        pass
+
+class nvg5268(gateway):
+    def __init__(self):
+        self.name="abc"
+        rg5268 = pexpect.spawn("telnet 192.168.1.254")
+        sleep(1)
+
