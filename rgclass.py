3abb0ec7084b3e05513d061fa50badf66ce5ed34
pfpalmer <pfpalmer@gmail.com>
2019-02-26 18:29:56 -0600

    test add a simple coment

--- rgclass.py
+++ rgclass.py
@@ -2,8 +2,13 @@ import pexpect
 import sys
 from time import sleep
 
+
+nvgInfo = { "228946241148656" : {'model':'nvg599','dac':"*<#/53#1/2", 'magic': 'kjundhkdxlxr','mac2g': 'd0:39:b3:60:56:f1','mac5g':'d0:39:b3:60:56:f4', 'wiFi': 'c2cmybt25dey','ssid': 'ATTqbrAnYs'},
+               "277427577103760" : {'model':'nvg599','dac': '<<01%//4&/','magic': "ggtxstgwipcg", 'mac2g': 'fc:51:a4:2f:25:90', 'mac5g': 'fc:51:a4:2f:25:94', 'wiFi': 'nsrmpr59rxwv', 'ssid' : 'ATTqbrAnYs'}}
+
 class  gateway:
     serialNumer=None
+    magic=None
 
 class nvg599(gateway):
     def __init__(self):
